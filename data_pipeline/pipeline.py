6dc9d89a7bf82a2c736fafdaf6f7baf209dd28f1
damlita <damla_atasayar@hotmail.de>
2021-03-26 14:38:00 +0100

    first commit

---
+++ data_pipeline/pipeline.py
@@ -0,0 +1,42 @@
+# ETL - Pipeline: Extract, Transform, Load
+# Output geht als Input in die Funktion ein in einem Loop
+
+from typing import Generator
+
+def read_large_dataset(file_name: str) -> Generator:
+    with open(file_name) as f:
+        for line in f:
+            yield line
+
+
+def split_lines(file_generator: Generator):
+    """Splitte Zeilen in eine Liste(kommaseparier"""
+    print("Split lines Generator: ", type(file_generator))
+    result = (line.strip().split(',') for line in file_generator)
+    return result
+
+
+def dictify(list_generator: Generator):
+    cols = next(list_generator) # spaltennamen der Datei
+    return (dict(zip(cols, data)) for data in list_generator)
+
+def load_data(file_name: str):
+    file_generator = read_large_dataset(file_name)
+    split_generator = split_lines(file_generator)
+    result = dictify(split_generator)
+    return result
+
+
+
+"""   
+    for element in d:
+        print(element)
+    print(d)
+    print(next(d))
+    print(next(d))
+    print(next(d))
+    print(next(d))
+    #print(next(split_generator))
+    #print(next(split_generator))
+
+"""
